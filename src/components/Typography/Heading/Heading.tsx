67633d5a78a86a70ce9f80f5d421ad85f329275e
Nick Galante <nicholasgalante1997@gmail.com>
2022-03-18 16:31:01 -0600

    im a dick

--- src/components/Typography/Heading/Heading.tsx
+++ src/components/Typography/Heading/Heading.tsx
@@ -4,7 +4,7 @@ import { HeadingProps } from './types';
 
 const Heading = (props: HeadingProps) => {
   const {
-    as, children, color, customStyles = {}, scale = 300,
+    as = 'h2', children, color, customStyles = {}, scale = 300,
   } = props;
   const Component = StyledDict[as];
   return (<Component style={customStyles} scale={scale} color={color}>{children}</Component>);
